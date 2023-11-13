f970ce148d9ac1943b921e6d87690427a0c1b695
Débora Mazzitelli <115846315+debora662@users.noreply.github.com>
2023-11-13 18:46:21 -0300

    update api

--- src/components/ItemListContainer/ItemListContainer.jsx
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -4,16 +4,14 @@ import axios from 'axios';
 import ItemList from '../ItemList/ItemList';
 
 const ItemListContainer = ({ greeting }) => {
-  const [products, setProducts] = useState([])
-  const [isLoading, setIsLoading] = useState(true)
+  const [products, setProducts] = useState([])  
 
   useEffect(() => {
     axios
       .get("https://api.mockfly.dev/mocks/03682ae5-1697-4466-835f-3c6f06ba02b1/products")
       .then(response => {
         const limitedProducts = response.data.slice(0, 24);
-        setProducts(limitedProducts);
-        setIsLoading(false)
+        setProducts(limitedProducts);        
       })
       .catch(error => window.alert(error.message));
   }, []);
